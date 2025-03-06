ec71bd741a7be852e153f4b7922005c4f044ce03
jose angel <jose.albelt1817@gmail.com>
2025-03-06 10:18:58 -0500

    simple form add

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -16,6 +16,9 @@ interface Characters {
 })
 export class DragonballComponent {
 
+  name = signal('');
+  power = signal(0);
+
   characters = signal<Characters[]>([
     {id: 0,name: 'Goku',power: 9001},
     {id: 1,name: 'Vegeta',power: 8008},
@@ -23,4 +26,24 @@ export class DragonballComponent {
     {id: 3,name: 'yamchan',power: 500},
   ]);
 
+
+
+  addCharacter() {
+    if(!this.name() || !this.power() || this.power() <= 0 ){
+      return;
+    }
+    const newCharacter: Characters = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+    // this.characters().push(newCharacter); metodo tradicional
+    this.characters.update((list) => [...list,newCharacter]);
+    this.restField();
+  }
+
+  restField(){
+    this.name.set('');
+    this.power.set(0)
+  }
 }
