66609aa291c7f7ad19a15266d133772f9ecf6c51
jose angel <jose.albelt1817@gmail.com>
2025-03-07 09:55:35 -0500

    local storage

--- src/app/services/drangonball.service.ts
+++ src/app/services/drangonball.service.ts
@@ -1,15 +1,23 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Characters } from '../interfaces/character.interfaces';
+
+// funcion aparate para el local storage
+const loadFromLocalStorage = ():Characters[] => {
+  const character =localStorage.getItem('characters');
+  return character ? JSON.parse(character): [];
+} 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DrangonballService {
+  
+  characterss = signal<Characters[]>(loadFromLocalStorage());
 
-
-  characterss = signal<Characters[]>([
-    {id: 0,name: 'Goku',power: 9001},
-  ]);
+  saveToLocalStores = effect( () => {
+    // console.log(`Character count:  ${this.characterss().length}`)
+    localStorage.setItem('characters',JSON.stringify(this.characterss()));  
+  })
 
   addCharacter(character: Characters){
   this.characterss.update( (list) => [...list,character]);
